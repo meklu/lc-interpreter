3508235d3fea42fcda98fecd68cd76ded166c5d4
Melker Narikka <melker.narikka@helsinki.fi>
2018-05-18 15:40:46 +0300

    Initial tokenizer

--- src/Tokenize.hs
+++ src/Tokenize.hs
@@ -2,7 +2,8 @@ module Tokenize where
 
 import STree
 
-data Token = Lambda | Abstr | BraceLeft | BraceRight
+data Token = Lambda | Abstr | BraceLeft | BraceRight | Var String
+  deriving Show
 
 lambdaToken :: Char
 lambdaToken = '\\'
@@ -16,9 +17,27 @@ braceLeft = '('
 braceRight :: Char
 braceRight = ')'
 
-whitespace :: [Char]
-whitespace = "\t\n\r "
+whitespaceChars :: [Char]
+whitespaceChars = "\t\n\r "
 
-generateTree :: String -> Expression
+variableChars :: [Char]
+variableChars = ['a'..'z'] ++ ['A'..'Z']
+
+isWhitespace :: Char -> Bool
+isWhitespace x = x `elem` whitespaceChars
+
+isVariable :: Char -> Bool
+isVariable x = x `elem` variableChars
+
+tokenizeExpr :: String -> [Token]
+tokenizeExpr []                           = []
+tokenizeExpr (x:xs) | isWhitespace x      = tokenizeExpr xs
+                    | x == lambdaToken    = Lambda     : tokenizeExpr xs
+                    | x == abstrToken     = Abstr      : tokenizeExpr xs
+                    | x == braceLeft      = BraceLeft  : tokenizeExpr xs
+                    | x == braceRight     = BraceRight : tokenizeExpr xs
+                    | isVariable x        = Var [x]    : tokenizeExpr xs
+tokenizeExpr (x:_)                        = error $ "Unrecognized token " ++ [x] ++ "!"
+
+generateTree :: [Token] -> Expression
 generateTree = undefined
-
