36fbf7edced5d00e9deedbae6af7d8465d43d242
Melker Narikka <melker.narikka@helsinki.fi>
2018-05-21 13:25:30 +0300

    Sanitize token names
    They're parens, not braces. Identifiers are identifiers. You get the
    idea.

--- src/Tokenize.hs
+++ src/Tokenize.hs
@@ -1,6 +1,6 @@
 module Tokenize where
 
-data Token = Lambda | Abstr | BraceLeft | BraceRight | Var String
+data Token = Lambda | Period | ParenLeft | ParenRight | Identifier String
   deriving Show
 
 lambdaToken :: Char
@@ -9,11 +9,11 @@ lambdaToken = '\\'
 abstrToken :: Char
 abstrToken = '.'
 
-braceLeft :: Char
-braceLeft = '('
+parenLeft :: Char
+parenLeft = '('
 
-braceRight :: Char
-braceRight = ')'
+parenRight :: Char
+parenRight = ')'
 
 whitespaceChars :: [Char]
 whitespaceChars = "\t\n\r "
@@ -38,14 +38,14 @@ tokenizeExpr :: String -> [Token]
 tokenizeExpr []                           = []
 tokenizeExpr (x:xs) | isWhitespace x      = tokenizeExpr xs
                     | x == lambdaToken    = Lambda     : tokenizeExpr xs
-                    | x == abstrToken     = Abstr      : tokenizeExpr xs
-                    | x == braceLeft      = BraceLeft  : tokenizeExpr xs
-                    | x == braceRight     = BraceRight : tokenizeExpr xs
-                    | isVariable x        = Var [x]    : tokenizeExpr xs
-                    | x == longVarToken   = longVar "" xs where
-                      longVar :: String -> String -> [Token]
-                      longVar _ []                                 = error "Unterminated long var!"
-                      longVar s (x:xs) | x == longVarToken         = Var s : tokenizeExpr xs
-                                       | x `elem` longVarWhitelist = longVar (s ++ [x]) xs
+                    | x == abstrToken     = Period     : tokenizeExpr xs
+                    | x == parenLeft      = ParenLeft  : tokenizeExpr xs
+                    | x == parenRight     = ParenRight : tokenizeExpr xs
+                    | isVariable x        = Identifier [x]    : tokenizeExpr xs
+                    | x == longVarToken   = longIdentifier "" xs where
+                      longIdentifier :: String -> String -> [Token]
+                      longIdentifier _ []                                 = error "Unterminated long var!"
+                      longIdentifier s (x:xs) | x == longVarToken         = Identifier s : tokenizeExpr xs
+                                       | x `elem` longVarWhitelist = longIdentifier (s ++ [x]) xs
                                        | otherwise                 = error $ "Illegal long var character " ++ [x,'!']
 tokenizeExpr (x:_)                        = error $ "Unrecognized token " ++ [x,'!']
